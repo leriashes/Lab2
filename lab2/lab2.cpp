9eaf2df53365d570a65143487996f648542305ed
Valeria Shevchenko <valeriya-shevchenko-2002@mail.ru>
2021-09-10 10:07:16 +0700

    Создание 4 структур (книга, автор, читатель, издательство), добавление элементов структуры Книга

---
+++ lab2/lab2.cpp
@@ -0,0 +1,32 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <conio.h>
+#include <locale.h>
+using namespace std;
+
+struct author {
+
+};
+
+struct reader {
+
+};
+
+struct publishing {
+
+};
+
+struct book {
+	string title;
+	author author_info;
+	int pages_number;
+	string genre;
+	reader reader_info;
+	publishing publ_info;
+	int inv_number;
+	int publ_year;
+};
+
+int main() {
+
+}
