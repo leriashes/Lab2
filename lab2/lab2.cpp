4e09e3e22090a50b696a895164395d1b0dd02e3f
Valeria Shevchenko <valeriya-shevchenko-2002@mail.ru>
2021-09-12 14:14:52 +0700

    Меню

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -32,7 +32,7 @@ struct reader {
 	string full_name;	//ФИО
 	date birth;			//Дата рождения
 	date book_get;		//Дата взятия книги
-	address r_address;		//Адрес
+	address r_address;	//Адрес
 };
 
 //Издательство
@@ -57,12 +57,42 @@ int main() {
 	setlocale(LC_ALL, "Rus");	//Для вывода на русском языке
 
 	book *books;
-	publishing publishings;
+	publishing *publishings;
 	reader *readers;
 	author *authors;
 
 	do {
-		;
+		system("cls");
+		printf("demo Билиотека\n\n");
+
+		char act;
+		printf("1 Добавить новую книгу\n2 Добавить нового читателя\n3 Добавить нового писателя\n4 Добавить новове издательство\n5 Взятие книги\n6 Сдача книги\n\n7 Выход\n\nВведите номер действия ");
+		do {
+			act = _getch();
+		} while (act < '1' || act > '7');
+
+		act -= 48;
+		if (act == 1) {
+
+		}
+		else if (act == 2) {
+			;
+		}
+		else if (act == 3) {
+			;
+		}
+		else if (act == 4) {
+
+		}
+		else if (act == 5) {
+			;
+		}
+		else if (act == 6) {
+			;
+		}
+		else {
+			printf("\n\nВы уверены, что хотите выйти? (Для выхода нажмите esc, для продолжения - любую клавишу.) ");
+		}
 	} while (_getch() != 27);
 
 	return 0;
