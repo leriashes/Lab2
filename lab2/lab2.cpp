15d2560bf0344727cd82a046583616abc5a5ebcb
Valeria Shevchenko <valeriya-shevchenko-2002@mail.ru>
2021-09-12 16:14:36 +0700

    Ввод и печать информации об издательстве

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 #include <conio.h>
 #include <locale.h>
+#include <stdio.h>
+#include <string>
+#include <windows.h>
 using namespace std;
 
 //Дата
@@ -11,11 +14,18 @@ struct date {
 	int year;	//Год
 };
 
+//Установка значений по умолчанию
+date setDate() {
+	date s_date;
+	s_date.day = s_date.month = s_date.year = 0;
+	return s_date;
+}
+
 //Автор
 struct author {
 	string full_name;	//ФИО
 	date birth;			//Дата рождения
-	date death;			//Дата смерти?
+	date death;			//Дата смерти
 	string country;		//Страна
 };
 
@@ -31,15 +41,36 @@ struct address {
 struct reader {
 	string full_name;	//ФИО
 	date birth;			//Дата рождения
-	date book_get;		//Дата взятия книги
 	address r_address;	//Адрес
+	int doc_number;		//Номер документа, удостоверяющего личность
 };
+
 
 //Издательство
 struct publishing {
 	string name;	//Название
 	string city;	//Город
 };
+
+//Ввод информации об издательстве
+publishing enterPublishing() {
+	publishing e_publishing;
+	setlocale(LC_ALL, "Rus");
+	printf("Введите название издательства: ");
+	getline(cin, e_publishing.name);
+
+	printf("\nВведите город, в котором находится издательство: ");
+	getline(cin, e_publishing.city);
+
+	return e_publishing;
+}
+
+//Печать информации об издательстве
+void printPublishing(publishing p_publishing) {
+	cout << p_publishing.name << ", " << p_publishing.city;
+	return;
+}
+
 
 //Книга
 struct book {
@@ -55,15 +86,22 @@ struct book {
 
 int main() {
 	setlocale(LC_ALL, "Rus");	//Для вывода на русском языке
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 
 	book *books;
 	publishing *publishings;
 	reader *readers;
 	author *authors;
-
+	
 	do {
 		system("cls");
 		printf("demo Билиотека\n\n");
+
+		publishing p = enterPublishing();
+		printPublishing(p);
+		string newfilm;
+		getline(cin, newfilm);
 
 		char act;
 		printf("1 Добавить новую книгу\n2 Добавить нового читателя\n3 Добавить нового писателя\n4 Добавить новове издательство\n5 Взятие книги\n6 Сдача книги\n\n7 Выход\n\nВведите номер действия ");
@@ -73,7 +111,7 @@ int main() {
 
 		act -= 48;
 		if (act == 1) {
-
+			;
 		}
 		else if (act == 2) {
 			;
@@ -93,6 +131,7 @@ int main() {
 		else {
 			printf("\n\nВы уверены, что хотите выйти? (Для выхода нажмите esc, для продолжения - любую клавишу.) ");
 		}
+
 	} while (_getch() != 27);
 
 	return 0;
